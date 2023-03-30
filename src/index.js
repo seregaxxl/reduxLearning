85e3c49dc23beead730eb2ced9f82b343ddbf4a8
seregaxxl <seregaxxl@gmail.com>
2023-03-30 16:43:45 +0300

    react+redux learning

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
-import * as actions from './actions'
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
 import reducer from './reducer';
+import App from './components/App';
 
 const store = createStore(reducer);
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value
-}
-
-subscribe(update)
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
-// const decDispatch = bindActionCreators(dec, dispatch)
-// const rndDispatch = bindActionCreators(rnd, dispatch)
-
-
-document.getElementById('inc').addEventListener('click', inc)
-
-document.getElementById('dec').addEventListener('click', dec)
-
-document.getElementById('rnd').addEventListener('click', ()=>{
-  const value = Math.floor(Math.random() * 10)
-  rnd(value)
-})
-
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <>
-    
-    </>
+
+    <Provider store={store}>
+      <App/>
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
+
+
